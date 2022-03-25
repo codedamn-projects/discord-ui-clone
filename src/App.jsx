74c5a3b560266c8351af707319d9346038f10014
Pranav <pranav.techiegeek@gmail.com>
2022-03-25 15:08:59 +0530

    converted to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-
+import React from "react";
+import discordLogo from "../assets/discord_main_logo.svg";
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload!
-				</p>
-				<span className="App-link">Hello from codedamn :)</span>
-			</header>
-		</div>
-	)
+  return (
+    <div style={{ backgroundColor: "#5865f2" }}>
+      <img src={discordLogo} alt="discord" />
+    </div>
+  );
 }
 
-export default App
+export default App;
